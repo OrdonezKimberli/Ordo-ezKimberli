9294870708809cfbcac92db2ac479c2e95ae3612
OrdonezKimberli <izahorikimberlio@gmail.com>
2025-05-06 21:14:05 -0600

    Update tkinter.py

--- tkinter.py
+++ tkinter.py
@@ -2,19 +2,23 @@ import tkinter as tk
 
 def saludar():
     nombre = entrada.get()
-    etiqueta_resultado.config(text=f"Hola{nombre}")
+    edad = entrada.get()
+    etiqueta_resultado.config(text=f"Hola{nombre,edad}")
     
     ventana = tk.TK()
-    ventana.title("Saludo")
-    ventana.geometry("300x150")
+    ventana.title("Mi primera app gráfica")
+    ventana.geometry("400x200")
     
     etiqueta = tk.Label(ventana,text="Ingresa tu nombre:")
+    etiqueta.pack()
+
+    etiqueta = tk.Label(ventana,text="Ingresa tu edad:")
     etiqueta.pack()
     
     entrada = tk.Entry(ventana)
     entrada.pack()
     
-    boton = tk.Button(ventana,text="Saludar",command=saludar)
+    boton = tk.Button(ventana,text="Mostrar saludo",command=saludar,edad)
     boton.pack()
     
     etiqueta_resultado = tk.Label(ventana,text="")
